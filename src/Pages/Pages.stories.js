623d59d0137aefd1256de861baa7bdf09f4e146d
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-02-23 23:18:23 +0900

    Added register & recruit list stories

--- src/Pages/Pages.stories.js
+++ src/Pages/Pages.stories.js
@@ -6,10 +6,14 @@ import { linkTo } from '@storybook/addon-links';
 import addonBackgrounds from '@storybook/addon-backgrounds';
 
 import MainPage from './Mainpage/Mainpage';
+import Register from './Register/Register';
+import RecruitList from './Recruit/List/RecruitList';
 
 // TODO add page stories
 
 storiesOf('Pages', module)
   .addWithDoc('Main page', MainPage,
     'Main page(index.html)', () => <MainPage />
-  );
+  )
+  .add('Register first page', () => <Register />)
+  .add('Recruit List Page', () => <RecruitList />);
